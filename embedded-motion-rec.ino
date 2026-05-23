683f417dcf197f1b9785e002e4eab930d6ffa340
nickaan04 <nickaan04@gmail.com>
2026-05-22 18:22:21 -0700

    tilt left data complete

--- embedded-motion-rec.ino
+++ embedded-motion-rec.ino
@@ -16,7 +16,7 @@
 // CHANGE THIS FOR EACH COLLECTION SESSION
 // Must match COLLECTION_LABEL in collect_data.py on your computer
 // Valid values: "still", "shake", "tilt left", "tilt right", "tilt forward", "tilt backward"
-const char CURRENT_LABEL[] = "tilt forward";
+const char CURRENT_LABEL[] = "tilt right";
 
 // Sampling configuration
 // One training example = 1 second of accelerometer data at 50 Hz = 50 rows.
